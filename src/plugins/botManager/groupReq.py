ae67ba4f5807a04d763108b6552103b6c9599217
Doeca <i@doeca.cc>
2023-09-30 04:26:19 +0800

    [new]:加入邀请管理

---
+++ src/plugins/botManager/groupReq.py
@@ -0,0 +1,9 @@
+﻿from nonebot import on_request
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11 import Bot, GroupRequestEvent
+
+girq = on_request()
+@girq.handle()
+async def superUserApprove(bot: Bot, e: GroupRequestEvent):
+    if e.sub_type == 'invite':
+        await e.approve(bot)

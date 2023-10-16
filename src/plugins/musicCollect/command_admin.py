e3e08892e769fd40288b38657114aa0e67e43d58
Doeca <i@doeca.cc>
2023-10-17 01:38:18 +0800

    [change]: commands for admins done transform

--- src/plugins/musicCollect/command_admin.py
+++ src/plugins/musicCollect/command_admin.py
@@ -6,9 +6,14 @@ from nonebot.adapters import Message
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg,  ArgStr
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent,PrivateMessageEvent
+from typing import Union
 from nonebot.permission import SUPERUSER
-from nonebot.adapters.onebot.v11 import GROUP_ADMIN, GROUP_OWNER
+from nonebot.internal.adapter import Bot, GROUP_ADMIN, GROUP_OWNER
+from nonebot.adapters.onebot.v11 import PrivateMessageEvent as v11PmsgEvent
+from nonebot.adapters.onebot.v12 import PrivateMessageEvent as v12PmsgEvent
+from nonebot.adapters.onebot.v11 import GroupMessageEvent as v11GmsgEvent
+from nonebot.adapters.onebot.v12 import GroupMessageEvent as v12GmsgEvent
+
 
 volume_matcher = on_command("volume", permission=(
     SUPERUSER | GROUP_ADMIN | GROUP_OWNER), aliases={"音量"}, rule=util.group_checker)
@@ -28,7 +33,7 @@ async def volume_handle(bot: Bot, matcher: Matcher, args: Message = CommandArg()
 
 
 @volume_matcher.got("arg", prompt="请输入音量百分比，例如：70%")
-async def volume_got(e: GroupMessageEvent, arg: str = ArgStr('arg')):
+async def volume_got(e: Union[v11GmsgEvent, v12GmsgEvent], arg: str = ArgStr('arg')):
     school_id = await config.get_id(str(e.group_id))
     rarg = arg.strip()
     volume = rarg
@@ -48,17 +53,30 @@ cookie_matcher = on_command("cookie", permission=SUPERUSER)
 
 
 @cookie_matcher.handle()
-async def cookieUploadPre(bot: Bot, matcher: Matcher, args: Message = CommandArg()):
+async def cookieUploadPre(bot: Bot, e: v11PmsgEvent, matcher: Matcher, args: Message = CommandArg()):
     cookie = args.extract_plain_text().strip()
     if cookie != '':
         matcher.set_arg("arg", cookie)
 
 
 @cookie_matcher.got("arg", prompt="请输入cookie文本")
-async def cookieUpload(bot: Bot, e: PrivateMessageEvent, arg: str = ArgStr('arg')):
+async def cookieUpload(bot: Bot, e: v11PmsgEvent, arg: str = ArgStr('arg')):
     apiUrl = config.system.music_api
     raw = str(base64.b64encode(arg.encode("utf-8")), "utf-8")
     await util.httpGet(f"{apiUrl}/update_cookie?raw={raw}")
     await cookie_matcher.finish(f"Cookies：{arg}\n已经上传")
+
+
+glist_matcher = on_command("glist",permission=SUPERUSER)
+
+
+@glist_matcher.handle()
+async def glist_handle(bot: Bot, e: v12PmsgEvent):
+    group_list = await bot.get_group_list()
+    resp = ""
+    for id in range(0, len(group_list)):
+        g = group_list[id]
+        resp += f"{id}. {g['group_name']}：{g['group_id']}\n"
+    await bot.send(e, message=resp)
 
 logger.info("管理端命令加载完成")

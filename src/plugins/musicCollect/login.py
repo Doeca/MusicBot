1178180857b703ce9995f272176f7c97cf76ad62
Doeca <i@doeca.cc>
2023-10-23 23:24:57 +0800

    [new]:加入了qq音乐二维码登录获取cookie的功能

--- src/plugins/musicCollect/login.py
+++ src/plugins/musicCollect/login.py
@@ -5,6 +5,7 @@ import time
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
+options.add_argument('--disable-dev-shm-usage')
 browser = None
 temp_str = ""
 

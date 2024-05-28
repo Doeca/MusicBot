25aa4c1768f01648e72658b0d76fbf4bf918b2ec
Doeca <i@doeca.cc>
2024-05-28 16:25:31 +0800

    [new]:prepare for new interface

---
+++ src/plugins/Anime/user.py
@@ -0,0 +1,6 @@
+from nonebot import on_command
+from nonebot.adapters import Message
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import Bot,  PrivateMessageEvent
+
